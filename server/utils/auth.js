063ff4f477e127a4fafb9289df95c9bb82afd4a6
ismail saoudi <ismail.saoudi@icloud.com>
2022-10-19 13:54:18 -0400

    troubleshooting signupform

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
-const secret = 'mysecretssshhhhhhh';
-const expiration = '2h';
+const secret = 'mysecretismysecret';
+const expiration = '1h';
 
 module.exports = {
   authMiddleware: function ({ req }) {
